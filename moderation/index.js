bd5d8449fb18e949ee0890838e3853d1d3b91e23
Jose Villaverde <jlvbalsa@gmail.com>
2022-08-28 16:50:18 +0200

    moderation Event Sync

--- moderation/index.js
+++ moderation/index.js
@@ -5,6 +5,25 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const handleEvent = async (type, data) => {
+  if (type === 'CommentCreated') {
+    console.log("Retrieven old event: " + type);
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    }).catch((err) => {
+      console.log(err.message);
+    });
+  }
+
+};
+
 app.post('/events', async (req, res) => {
   const { type, data } = req.body;
 
@@ -27,6 +46,17 @@ app.post('/events', async (req, res) => {
   res.send({});
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
   console.log('Listening on 4003');
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
